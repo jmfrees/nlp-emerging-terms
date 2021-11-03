2570ecddeb385ce255d0933e9eb322076a9635bc
Jonathan Frees <jmfrees@uab.edu>
2021-11-03 16:40:33 -0500

    Correct sentence yield functionality

--- semantic_extraction/preprocess.py
+++ semantic_extraction/preprocess.py
@@ -1,3 +1,6 @@
+# STL
+import subprocess
+
 # PDM
 import nltk
 from tqdm import tqdm
@@ -15,15 +18,19 @@ class CorpusReader:
         self.fp = filepath
         self.tokenizer = TweetTokenizer()
 
+        self.linecount = int(subprocess.check_output(["wc", "-l", filepath]).split()[0])
+
     def __iter__(self):
         with open(self.fp, "r", encoding="utf-8") as f:
-            for line in tqdm(f):
+            for line in tqdm(f, total=self.linecount):
                 if not line:  # ingore blank lines
                     continue
                 for s in sent_tokenize(line):
-                    yield from filter(
-                        lambda w: w not in eng_stopwords,
-                        self.tokenizer.tokenize(s),
+                    yield list(
+                        filter(
+                            lambda w: w not in eng_stopwords,
+                            self.tokenizer.tokenize(s),
+                        )
                     )
 
 
