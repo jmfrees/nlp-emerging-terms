77d514331681d6fee39ba36530de3fd6daf35130
Jonathan Frees <jmfrees@uab.edu>
2021-11-23 15:22:43 -0600

    Use basic SBERT cos-similarity

--- semantic_extraction/semantic_search_hnswlib.py
+++ semantic_extraction/semantic_search_hnswlib.py
@@ -80,29 +80,29 @@ else:
         corpus_sentences = cache_data["sentences"]
         corpus_embeddings = cache_data["embeddings"]
 
-# Defining our hnswlib index
-index_path = "./hnswlib.index"
-# We use Inner Product (dot-product) as Index. We will normalize our vectors to unit length, then is Inner Product equal to cosine similarity
-index = hnswlib.Index(space="cosine", dim=embedding_size)
+# # Defining our hnswlib index
+# index_path = "./hnswlib.index"
+# # We use Inner Product (dot-product) as Index. We will normalize our vectors to unit length, then is Inner Product equal to cosine similarity
+# index = hnswlib.Index(space="cosine", dim=embedding_size)
 
-if os.path.exists(index_path):
-    print("Loading index...")
-    index.load_index(index_path)
-else:
-    ### Create the HNSWLIB index
-    print("Start creating HNSWLIB index")
-    index.init_index(max_elements=len(corpus_embeddings), ef_construction=400, M=64)
+# if os.path.exists(index_path):
+#     print("Loading index...")
+#     index.load_index(index_path)
+# else:
+#     # Create the HNSWLIB index
+#     print("Start creating HNSWLIB index")
+#     index.init_index(max_elements=len(corpus_embeddings), ef_construction=400, M=64)
 
-    # Then we train the index to find a suitable clustering
-    index.add_items(corpus_embeddings, list(range(len(corpus_embeddings))))
+#     # Then we train the index to find a suitable clustering
+#     index.add_items(corpus_embeddings, list(range(len(corpus_embeddings))))
 
-    print("Saving index to:", index_path)
-    index.save_index(index_path)
+#     print("Saving index to:", index_path)
+#     index.save_index(index_path)
 
-# Controlling the recall by setting ef:
-index.set_ef(50)  # ef should always be > top_k_hits
+# # Controlling the recall by setting ef:
+# index.set_ef(50)  # ef should always be > top_k_hits
 
-######### Search in the index ###########
+# Search in the index
 
 print("Corpus loaded with {} sentences / embeddings".format(len(corpus_sentences)))
 
@@ -112,49 +112,52 @@ while True:
     start_time = time.time()
     question_embedding = model.encode(inp_question)
 
-    # We use hnswlib knn_query method to find the top_k_hits
-    corpus_ids, distances = index.knn_query(question_embedding, k=top_k_hits)
+    # # We use hnswlib knn_query method to find the top_k_hits
+    # corpus_ids, distances = index.knn_query(question_embedding, k=top_k_hits)
 
-    # We extract corpus ids and scores for the first query
-    hits = [
-        {"corpus_id": id, "score": 1 - score}
-        for id, score in zip(corpus_ids[0], distances[0])
-    ]
-    hits = sorted(hits, key=lambda x: x["score"], reverse=True)
-    end_time = time.time()
+    # # We extract corpus ids and scores for the first query
+    # hits = [
+    #     {"corpus_id": id, "score": 1 - score}
+    #     for id, score in zip(corpus_ids[0], distances[0])
+    # ]
+    # hits = sorted(hits, key=lambda x: x["score"], reverse=True)
+    # end_time = time.time()
 
     print("Input question:", inp_question)
-    print("Results (after {:.3f} seconds):".format(end_time - start_time))
-    for hit in hits[0:top_k_hits]:
-        print("\t{:.3f}\t{}".format(hit["score"], corpus_sentences[hit["corpus_id"]]))
+    # print("Results (after {:.3f} seconds):".format(end_time - start_time))
+    # for hit in hits[0:top_k_hits]:
+    #     print("\t{:.3f}\t{}".format(hit["score"], corpus_sentences[hit["corpus_id"]]))
 
     # Approximate Nearest Neighbor (ANN) is not exact, it might miss entries with high cosine similarity
     # Here, we compute the recall of ANN compared to the exact results
     correct_hits = util.semantic_search(
         question_embedding, corpus_embeddings, top_k=top_k_hits
     )[0]
-    correct_hits_ids = set([hit["corpus_id"] for hit in correct_hits])
+    for hit in correct_hits[0:top_k_hits]:
+        print("\t{:.3f}\t{}".format(hit["score"], corpus_sentences[hit["corpus_id"]]))
 
-    ann_corpus_ids = set([hit["corpus_id"] for hit in hits])
-    if len(ann_corpus_ids) != len(correct_hits_ids):
-        print(
-            "Approximate Nearest Neighbor returned a different number of results than expected"
-        )
+    # correct_hits_ids = set([hit["corpus_id"] for hit in correct_hits])
 
-    recall = len(ann_corpus_ids.intersection(correct_hits_ids)) / len(correct_hits_ids)
-    print(
-        "\nApproximate Nearest Neighbor Recall@{}: {:.2f}".format(
-            top_k_hits, recall * 100
-        )
-    )
+    # ann_corpus_ids = set([hit["corpus_id"] for hit in hits])
+    # if len(ann_corpus_ids) != len(correct_hits_ids):
+    #     print(
+    #         "Approximate Nearest Neighbor returned a different number of results than expected"
+    #     )
 
-    if recall < 1:
-        print("Missing results:")
-        for hit in correct_hits[0:top_k_hits]:
-            if hit["corpus_id"] not in ann_corpus_ids:
-                print(
-                    "\t{:.3f}\t{}".format(
-                        hit["score"], corpus_sentences[hit["corpus_id"]]
-                    )
-                )
-    print("\n\n========\n")
+    # recall = len(ann_corpus_ids.intersection(correct_hits_ids)) / len(correct_hits_ids)
+    # print(
+    #     "\nApproximate Nearest Neighbor Recall@{}: {:.2f}".format(
+    #         top_k_hits, recall * 100
+    #     )
+    # )
+
+    # if recall < 1:
+    #     print("Missing results:")
+    #     for hit in correct_hits[0:top_k_hits]:
+    #         if hit["corpus_id"] not in ann_corpus_ids:
+    #             print(
+    #                 "\t{:.3f}\t{}".format(
+    #                     hit["score"], corpus_sentences[hit["corpus_id"]]
+    #                 )
+    #             )
+    # print("\n\n========\n")
