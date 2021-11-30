08e2ca9c3a1a78451bf3000aeef6fbe196bac91e
Jonathan Frees <jmfrees@uab.edu>
2021-11-30 13:04:42 -0600

    Add debugging print statements

--- semantic_extraction/validate.py
+++ semantic_extraction/validate.py
@@ -14,6 +14,7 @@ def main(args):
     gold_standard_terms = set()
     with open(args.gold_standard, "r") as f:
         gold_standard_terms = set(line.strip() for line in f)
+    print(gold_standard_terms)
 
     # load w2v model and get top x terms related to our chosen term
     w2v_model = Word2Vec.load(args.model)
@@ -28,11 +29,17 @@ def main(args):
     top_terms_set = set(
         str(term[0]) for term in top_terms
     )  # cast to `str` is just for type checking
+    print(top_terms)
+    print(top_terms_set)
     missing_terms = gold_standard_terms - top_terms_set
     extra_terms: Set[str] = top_terms_set - gold_standard_terms
+    print(missing_terms)
+    print(extra_terms)
     precision = len(top_terms_set & gold_standard_terms) / len(top_terms_set)
     recall = len(top_terms_set & gold_standard_terms) / len(gold_standard_terms)
-    f1 = 2 * precision * recall / (precision + recall)
+    f1 = 0
+    if (precision + recall) != 0:
+        f1 = 2 * precision * recall / (precision + recall)
     print("Precision: {}".format(precision))
     print("Recall: {}".format(recall))
     print("F1: {}".format(f1))
@@ -51,6 +58,7 @@ if __name__ == "__main__":
         default="semantic_extraction/models/w2v_model.model",
     )
     parser.add_argument(
+        "-T",
         "--gold-standard",
         type=str,
         default="semantic_extraction/gold_standard.txt",
@@ -60,7 +68,7 @@ if __name__ == "__main__":
         "-o",
         "--output",
         type=str,
-        default="semantic_extraction/output.txt",
+        default="output.txt",
     )
     parser.add_argument(
         "--cosine-similarity-threshold",
@@ -68,6 +76,7 @@ if __name__ == "__main__":
         default=0.5,
     )
     parser.add_argument(
+        "-t",
         "--term",
         type=str,
         default="",
